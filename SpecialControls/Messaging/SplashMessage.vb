8d0188942409aa3f35d8a514cf63e14f4115e22a
Hanyu <8245snake@gmail.com>
2021-08-01 02:44:48 +0900

    不要な処理やクラスを削除

--- SpecialControls/Messaging/SplashMessage.vb
+++ SpecialControls/Messaging/SplashMessage.vb
@@ -2,8 +2,7 @@
 Option Strict On
 
 Imports System.Drawing.Drawing2D
-Imports System.Threading
-Imports SpecialControls.Win32
+
 
 Namespace Messaging
 
@@ -12,7 +11,6 @@ Namespace Messaging
     ''' スプラッシュメッセージを表示するクラス
     ''' </summary>
     Public Class SplashMessage
-
 
         ''' <summary>
         ''' スプラッシュメッセージが消えるまでの時間
@@ -31,101 +29,6 @@ Namespace Messaging
             ''' </summary>
             Endless = -1
         End Enum
-
-        Private _Bitmap As Bitmap
-        Private _X As Integer
-        Private _Y As Integer
-        Private _Duration As DisplayDuration
-        Private _IsCancel As Boolean = False
-
-        ''' <summary>
-        ''' 常に表示し続けるか
-        ''' </summary>
-        Private ReadOnly Property IsEndless As Boolean
-            Get
-                Return _Duration <= 0
-            End Get
-        End Property
-
-
-        ''' <summary>
-        ''' スプラッシュメッセージを表示する
-        ''' </summary>
-        ''' <param name="bmp">表示するビットマップ</param>
-        ''' <param name="x">X座標</param>
-        ''' <param name="y">Y座標</param>
-        ''' <param name="duration">表示時間</param>
-        Public Sub Show(bmp As Bitmap, x As Integer, y As Integer, Optional duration As DisplayDuration = DisplayDuration.ShortTime)
-            _Bitmap = bmp
-            _X = x
-            _Y = y
-            _Duration = duration
-            _IsCancel = False
-            Dim t As New Thread(New ThreadStart(AddressOf DoThread))
-            t.IsBackground = True
-            t.Start()
-        End Sub
-
-        ''' <summary>
-        ''' 表示中のスプラッシュメッセージを閉じる
-        ''' </summary>
-        Public Sub Hide()
-            _IsCancel = True
-            _Bitmap = Nothing
-        End Sub
-
-        Private Sub DoThread()
-            showSplashMessage(_Bitmap, _X, _Y)
-        End Sub
-
-        ''' <summary>
-        '''  ビットマップを指定した座標に表示する
-        ''' </summary>
-        ''' <param name="bmp"></param>
-        ''' <param name="x"></param>
-        ''' <param name="y"></param>
-        Protected Overridable Sub showSplashMessage(bmp As Bitmap, x As Integer, y As Integer)
-
-            ' Zeroを指定するとデスクトップのデバイスコンテキストを取得できる
-            Dim hdc As IntPtr = GetDC(IntPtr.Zero)
-            Dim hsrc As IntPtr = CreateCompatibleDC(hdc)
-            SelectObject(hsrc, bmp.GetHbitmap())
-
-            Dim width As Integer = bmp.Width
-            Dim height As Integer = bmp.Height
-
-            ' 少しずつ透過度を上げて表示
-            AlphaBlend(hdc, x, y, width, height, hsrc, 0, 0, width, height, 100 * &H10000)
-            Thread.Sleep(50)
-            AlphaBlend(hdc, x, y, width, height, hsrc, 0, 0, width, height, 180 * &H10000)
-            Thread.Sleep(50)
-
-            Dim total As Long = 0
-            Dim sleepMilliseconds As Integer = 10
-            If IsEndless Then
-                total = -100
-            End If
-            While Not _IsCancel AndAlso total < _Duration
-                ' 透過する必要ないのでBitBlt
-                BitBlt(hdc, x, y, width, height, hsrc, 0, 0, TernaryRasterOperations.SRCCOPY)
-                Thread.Sleep(sleepMilliseconds)
-                If Not IsEndless Then
-                    total += sleepMilliseconds
-                End If
-            End While
-
-            ' 後始末
-            DeleteDC(hsrc)
-            ReleaseDC(IntPtr.Zero, hdc)
-
-            ' メッセージを消す
-            Dim rect As RECT
-            rect.Left = x
-            rect.Top = y
-            rect.Right = x + width
-            rect.Bottom = y + height
-            InvalidateRect(IntPtr.Zero, rect, True)
-        End Sub
 
         ''' <summary>
         ''' テキストが書かれたビットマップを作成する
@@ -187,44 +90,6 @@ Namespace Messaging
             Return canvas
         End Function
 
-        ''' <summary>
-        ''' 指定したフォームのビットマップを作成する
-        ''' </summary>
-        ''' <param name="form">フォーム</param>
-        ''' <param name="clientAreaOnly">クライアント領域のみに限定するか</param>
-        ''' <returns>ビットマップ</returns>
-        Public Shared Function CreateFormImage(form As Form, Optional clientAreaOnly As Boolean = True) As Bitmap
-
-            Dim bmt As Bitmap = CaptureControl(form)
-
-            If clientAreaOnly Then
-                ' クライアント領域に切り詰める
-                Dim rect As Rectangle
-                rect.X = CInt((form.Size.Width - form.ClientSize.Width) / 2)
-                rect.Y = form.Size.Height - form.ClientSize.Height - rect.X
-                rect.Width = form.ClientSize.Width
-                rect.Height = form.ClientSize.Height
-                bmt = bmt.Clone(rect, bmt.PixelFormat)
-            End If
-
-            Return bmt
-        End Function
-
-        ''' <summary>
-        ''' コントロールをキャプチャする。
-        ''' DrawToBitmapでは不可能なリッチテキストコントロールにも対応。
-        ''' </summary>
-        ''' <param name="ctrl">コントロール</param>
-        ''' <returns>ビットマップ</returns>
-        Public Shared Function CaptureControl(ByVal ctrl As Control) As Bitmap
-            Dim img As New Bitmap(ctrl.Width, ctrl.Height)
-            Using memg As Graphics = Graphics.FromImage(img)
-                Dim dc As IntPtr = memg.GetHdc()
-                PrintWindow(ctrl.Handle, dc, 0)
-                memg.ReleaseHdc(dc)
-            End Using
-            Return img
-        End Function
 
     End Class
 
