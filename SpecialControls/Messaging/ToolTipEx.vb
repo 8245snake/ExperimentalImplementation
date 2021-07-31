31376994f3e1fd4a78a0c468a13872278d88195c
Hanyu <8245snake@gmail.com>
2021-08-01 02:00:58 +0900

    ツールチップ拡張クラス作成

---
+++ SpecialControls/Messaging/ToolTipEx.vb
@@ -0,0 +1,67 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+Imports SpecialControls.Messaging
+
+Public Class ToolTipEx
+    Inherits ToolTip
+
+    Private _Images As ConditionalWeakTable(Of Control, Bitmap)
+
+    Public Sub New()
+        MyBase.New()
+        _Images = New ConditionalWeakTable(Of Control, Bitmap)()
+        Me.OwnerDraw = True
+        AddHandler Me.Popup, AddressOf OnPopup
+        AddHandler Me.Draw, AddressOf OnDraw
+
+        Me.ShowAlways = True
+    End Sub
+
+    Public Sub SetToolTipEx(control As Control, caption As String, bmp As Bitmap)
+        Me.SetToolTip(control, caption)
+
+        If bmp IsNot Nothing Then
+            _Images.Add(control, bmp)
+        End If
+
+    End Sub
+
+    Public Sub SetToolTipEx(control As Control, caption As String, font As Font, Optional fontColor As Color = Nothing, Optional backColor As Color = Nothing)
+        Dim bmp = SplashMessage.CreateTextImage("クリアしました", font, fontColor, backColor)
+        SetToolTipEx(control, caption, bmp)
+    End Sub
+
+
+    Public Sub ShowBitmap(bitmap As Bitmap, window As IWin32Window, x As Integer, y As Integer, duration As Integer)
+        Dim control = TryCast(window, Control)
+        If control IsNot Nothing Then
+            _Images.Remove(control)
+            _Images.Add(control, bitmap)
+        End If
+        Me.Show("image", window, x, y, duration)
+    End Sub
+
+
+    Private Sub OnPopup(sender As Object, e As PopupEventArgs)
+        Dim bmp As Bitmap
+        If _Images.TryGetValue(e.AssociatedControl, bmp) Then
+            e.ToolTipSize = bmp.Size
+        End If
+    End Sub
+
+    Private Sub OnDraw(sender As Object, e As DrawToolTipEventArgs)
+        Dim bmp As Bitmap
+        If _Images.TryGetValue(e.AssociatedControl, bmp) Then
+            e.Graphics.DrawImage(bmp, e.Bounds.X, e.Bounds.Y)
+        Else
+            ' 画像なしなら普段通りの表示
+            e.DrawText()
+        End If
+    End Sub
+
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        RemoveHandler Me.Popup, AddressOf OnPopup
+        RemoveHandler Me.Draw, AddressOf OnDraw
+        MyBase.Dispose(disposing)
+    End Sub
+End Class
---
+++ SpecialControls/Messaging/ToolTipForm.vb
@@ -0,0 +1,75 @@
+﻿
+Imports SpecialControls.Win32
+
+Namespace Messaging
+    Public Class ToolTipForm
+        Inherits Form
+
+        Private _Bitmap As Bitmap
+
+        Protected Overrides ReadOnly Property ShowWithoutActivation As Boolean
+            Get
+                ' フォーカスを奪わないためにTrueとしたい
+                Return True
+            End Get
+        End Property
+
+        Sub New()
+            Me.StartPosition = FormStartPosition.Manual
+            Me.Location = New Point(-10000, -10000)
+            Me.ShowInTaskbar = False
+        End Sub
+
+        Protected Overrides Sub OnShown(e As EventArgs)
+            MyBase.OnShown(e)
+            _Bitmap = CreateFormImage(Me)
+            Me.Close()
+        End Sub
+
+        Public Function GetBitmap() As Bitmap
+            Me.ShowDialog()
+            Return _Bitmap
+        End Function
+
+        ''' <summary>
+        ''' 指定したフォームのビットマップを作成する
+        ''' </summary>
+        ''' <param name="form">フォーム</param>
+        ''' <param name="clientAreaOnly">クライアント領域のみに限定するか</param>
+        ''' <returns>ビットマップ</returns>
+        Public Shared Function CreateFormImage(form As Form, Optional clientAreaOnly As Boolean = True) As Bitmap
+
+            Dim bmt As Bitmap = CaptureControl(form)
+
+            If clientAreaOnly Then
+                ' クライアント領域に切り詰める
+                Dim rect As Rectangle
+                rect.X = CInt((form.Size.Width - form.ClientSize.Width) / 2)
+                rect.Y = form.Size.Height - form.ClientSize.Height - rect.X
+                rect.Width = form.ClientSize.Width
+                rect.Height = form.ClientSize.Height
+                bmt = bmt.Clone(rect, bmt.PixelFormat)
+            End If
+
+            Return bmt
+        End Function
+
+        ''' <summary>
+        ''' コントロールをキャプチャする。
+        ''' DrawToBitmapでは不可能なリッチテキストコントロールにも対応。
+        ''' </summary>
+        ''' <param name="ctrl">コントロール</param>
+        ''' <returns>ビットマップ</returns>
+        Public Shared Function CaptureControl(ByVal ctrl As Control) As Bitmap
+            Dim img As New Bitmap(ctrl.Width, ctrl.Height)
+            Using memg As Graphics = Graphics.FromImage(img)
+                Dim dc As IntPtr = memg.GetHdc()
+                PrintWindow(ctrl.Handle, dc, 0)
+                memg.ReleaseHdc(dc)
+            End Using
+            Return img
+        End Function
+
+
+    End Class
+End Namespace
