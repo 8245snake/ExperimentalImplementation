4ef93cd3f31f47fc85712b0974bf81bbe9353b80
Hanyu <8245snake@gmail.com>
2021-12-31 17:41:31 +0900

    計測開始からアイドル状態になるまでの時間を測定するサービスを作成

---
+++ DiagnosticLogging/DiagnosticLoggingService.vb
@@ -0,0 +1,72 @@
+﻿Option Explicit On
+Option Strict On
+
+''' <summary>
+''' 計測開始からアイドル状態になるまでの時間を測定するサービス
+''' </summary>
+Public Class DiagnosticLoggingService
+
+    Public Shared Instance As DiagnosticLoggingService = New DiagnosticLoggingService(Nothing)
+
+    Private _timer As Timer
+    Private _eventStartTick As Integer
+    Private _timerInterval As Integer = 100
+    Private _eventName As String = ""
+    Private _isReadyToStart As Boolean = True
+    Private _logger As IDiagnosticLogger
+
+    Public Sub New(logger As IDiagnosticLogger, Optional timerInterval As Integer = 100)
+        _logger = logger
+        _timer = New Timer()
+        _timer.Enabled = False
+        _timerInterval = timerInterval
+        _timer.Interval = timerInterval
+        AddHandler _timer.Tick, AddressOf Tick
+    End Sub
+
+    ''' <summary>
+    ''' 測定を開始する
+    ''' </summary>
+    ''' <param name="eventName">ログに出力される任意のイベント名</param>
+    Public Sub Start(eventName As String)
+        If Not _isReadyToStart Then
+            Return
+        End If
+        _eventName = eventName
+        _eventStartTick = Environment.TickCount
+        RemoveHandler Application.Idle, AddressOf OnIdle
+        AddHandler Application.Idle, AddressOf OnIdle
+        _isReadyToStart = False
+    End Sub
+
+    Private Sub OnIdle(sender As Object, e As EventArgs)
+        ' アイドル状態になったからといって一瞬のことかもしれないので{_timerInterval}ミリ秒だけログ出力を保留する
+        _timer.Enabled = True
+    End Sub
+
+    ''' <summary>
+    ''' アイドル状態になったあとしばらくして呼ばれる
+    ''' </summary>
+    Private Sub Tick(sender As Object, e As EventArgs)
+        _timer.Enabled = False
+        Dim millisecond As Integer = Environment.TickCount - _eventStartTick - _timerInterval
+        If _logger Is Nothing Then
+            System.Console.WriteLine($"[{_eventName}] {millisecond}ミリ秒")
+        Else
+            _logger.WriteLog(_eventName, millisecond)
+        End If
+        ' 後始末
+        RemoveHandler Application.Idle, AddressOf OnIdle
+        _isReadyToStart = True
+        _eventName = ""
+    End Sub
+End Class
+
+Public Interface IDiagnosticLogger
+    ''' <summary>
+    ''' 測定ログを出力する
+    ''' </summary>
+    ''' <param name="eventName">イベント名</param>
+    ''' <param name="millisecond">経過時間(ミリ秒)</param>
+    Sub WriteLog(eventName As String, millisecond As Integer)
+End Interface
---
+++ DiagnosticLogging/MainForm.vb
@@ -0,0 +1,32 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.Threading
+Imports System.Windows.Forms
+
+Public Class MainForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DiagnosticLoggingService.Instance.Start("Button1_Click")
+        Thread.Sleep(1000)
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DiagnosticLoggingService.Instance.Start("MainForm_Load")
+        Thread.Sleep(1000)
+    End Sub
+
+    Private Sub MainForm_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        DiagnosticLoggingService.Instance.Start("MainForm_Shown")
+        Dim timer As System.Windows.Forms.Timer = New System.Windows.Forms.Timer()
+        timer.Interval = 100
+        AddHandler timer.Tick, AddressOf Timer_Tick
+        timer.Enabled = True
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs)
+        DiagnosticLoggingService.Instance.Start("Timer_Tick")
+        Thread.Sleep(5000)
+        RemoveHandler DirectCast(sender, System.Windows.Forms.Timer).Tick, AddressOf Timer_Tick
+        DirectCast(sender, System.Windows.Forms.Timer).Enabled = False
+    End Sub
+End Class
